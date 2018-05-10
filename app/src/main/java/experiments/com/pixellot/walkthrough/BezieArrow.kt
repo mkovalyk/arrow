33a993039d24503e85e884763890a215af5ea72e
Mykhailo Kovalyk <mykhailo.kovalyk@globallogic.com>
2018-05-10 16:28:17 +0300

    Add ability to draw line.

---
+++ app/src/main/java/experiments/com/pixellot/walkthrough/BezieArrow.kt
@@ -0,0 +1,73 @@
+package experiments.com.pixellot.walkthrough
+
+import android.content.Context
+import android.graphics.*
+import android.util.AttributeSet
+import android.view.View
+
+
+/**
+ * Created on 10.05.18.
+ */
+class BezieArrow @JvmOverloads constructor(
+        context: Context, attrs: AttributeSet? = null, defStyleAttr: Int = 0
+) : View(context, attrs, defStyleAttr) {
+    private val start = PointF()
+    private val end = PointF()
+    private val firstMultiplier = PointF()
+    private val secondMultiplier = PointF()
+    private val paint: Paint = Paint(Paint.ANTI_ALIAS_FLAG)
+    private val path = Path()
+
+    init {
+        paint.style = Paint.Style.STROKE
+        paint.color = Color.parseColor("#f5a623")
+        paint.strokeWidth = 10f
+    }
+
+    fun setStart(x: Float, y: Float) {
+        start.x = x
+        start.y = y
+        evaluateMultipliers()
+    }
+
+    fun evaluateMultipliers() {
+        val resultPointX = (end.x + start.x) / 2
+        val resultPointY = (end.y + start.y) / 2
+
+        if (start.x == 0f) {
+            firstMultiplier.x = 1f
+        } else {
+            firstMultiplier.x = resultPointX / start.x
+        }
+        if (start.y == 0f) {
+            firstMultiplier.y = 1f
+        } else {
+            firstMultiplier.y = resultPointY / start.y
+        }
+        if (resultPointX == 0f) {
+            secondMultiplier.x = 1f
+        } else {
+            secondMultiplier.x = end.x / resultPointX
+        }
+        if (resultPointY == 0f) {
+            secondMultiplier.y = 1f
+        } else {
+            secondMultiplier.y = end.y / resultPointY
+        }
+    }
+
+    fun setEnd(x: Float, y: Float) {
+        end.x = x
+        end.y = y
+        evaluateMultipliers()
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        path.reset()
+        path.moveTo(start.x, start.y)
+        path.cubicTo(firstMultiplier.x * start.x, firstMultiplier.y * start.y, end.x / secondMultiplier.x, end.y / secondMultiplier.y, end.x, end.y)
+        canvas.drawPath(path, paint)
+        super.onDraw(canvas)
+    }
+}
--- app/src/main/java/experiments/com/pixellot/walkthrough/MainActivity.kt
+++ app/src/main/java/experiments/com/pixellot/walkthrough/MainActivity.kt
@@ -2,11 +2,77 @@ package experiments.com.pixellot.walkthrough
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
+import java.util.*
+
 
 class MainActivity : AppCompatActivity() {
+    val TAG = "MainActivity"
+    private val random = Random(100)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+//        button.setOnClickListener { drawArrow(random.nextInt(500).toFloat(), random.nextInt(500).toFloat()) }
+
+        arrow.post {
+            setStartForArrow()
+            setEndForArrow()
+        }
+        layout.setOnTouchListener { v, event -> buttonTouched(v, event) }
+    }
+
+    var oldX: Float = 0f
+    var oldY: Float = 0f
+
+    private fun buttonTouched(v: View, event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            oldX = event.x
+            oldY = event.y
+            return true
+        }
+        if (event.action == MotionEvent.ACTION_UP || event.action == MotionEvent.ACTION_POINTER_UP) {
+            drawArrow(event.x, event.y)
+        }
+        return false
+    }
+
+    private fun setEndForArrow() {
+        arrow.setEnd(button.x, button.y + button.height / 2)
+    }
+
+    private fun drawArrow(newX: Float, newY: Float) {
+        button.x = newX
+        button.y = newY
+        arrow.setEnd(button.x, button.y + button.height / 2)
+        arrow.invalidate()
+    }
+
+    private fun setStartForArrow() {
+        var x = getRelativeLeft(hint)
+        var y = getRelativeTop(hint)
+        Log.d(TAG, "X: $x Y: $y")
+        x += hint.width
+        y += hint.height / 2
+        arrow.setStart(x.toFloat(), y.toFloat())
+    }
+
+    private fun getRelativeLeft(myView: View): Int {
+        Log.d(TAG, "relativeLeft: $myView")
+        return if (myView.id == layout.id)
+            myView.left
+        else
+            myView.left + getRelativeLeft(myView.parent as View)
+    }
+
+    private fun getRelativeTop(myView: View): Int {
+        Log.d(TAG, "relativeTop: $myView")
+        return if (myView.id == layout.id)
+            myView.top
+        else
+            myView.top + getRelativeTop(myView.parent as View)
     }
 }
